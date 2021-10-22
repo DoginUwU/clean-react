67197c817e39060c55041925f9179e65feab9377
Luiz Eduardo <doginuwu@gmail.com>
2021-10-22 20:34:04 -0300

    chore: config sass

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,15 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+  collectCoverageFrom: [
+    'src/**/*.{ts,tsx}', '!**/*.d.ts'
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'jsdom',
   transform: {
     '^.+\\.tsx?$': 'ts-jest'
   },
   moduleNameMapper: {
-    '@/(.*)': '<rootDir>/src/$1'
+    '@/(.*)': '<rootDir>/src/$1',
+    '\\.(css|less|sass|scss)$': 'identity-obj-proxy'
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,57 @@
-const path = require('path');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-  mode: "development",
-  entry: "./src/main/index.tsx",
+  mode: 'development',
+  entry: './src/main/index.tsx',
   output: {
-    path: path.join(__dirname, "public/js"),
-    publicPath: "/public/js",
-    filename: "bundle.js",
+    path: path.join(__dirname, 'public/js'),
+    publicPath: '/public/js',
+    filename: 'bundle.js'
   },
   resolve: {
-    extensions: [".ts", ".tsx", ".js", ".jsx"],
+    extensions: ['.ts', '.tsx', '.js', '.jsx', 'scss'],
     alias: {
-      "@": path.join(__dirname, "src"),
-    },
+      '@': path.join(__dirname, 'src')
+    }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: 'style-loader'
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ]
+      }
+    ]
   },
   devServer: {
-    contentBase: path.join(__dirname, "public"),
-    publicPath: "/public/",
+    contentBase: path.join(__dirname, 'public'),
+    publicPath: '/public/',
     writeToDisk: true,
     historyApiFallback: true
   },
   externals: {
-    "react": "React",
-    "react-dom": "ReactDOM"
+    react: 'React',
+    'react-dom': 'ReactDOM'
   },
   plugins: [
     new CleanWebpackPlugin()
   ]
-};
+}

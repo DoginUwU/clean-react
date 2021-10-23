d0c22f1b60106b32aeba30c29dd7ec5d677f532a
Luiz Eduardo <doginuwu@gmail.com>
2021-10-23 00:43:48 -0300

    feat: remove autocomplete on inputs

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -1,11 +1,16 @@
+import { FocusEvent } from 'react';
 import Styles from './input-styles.scss'
 
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>;
 
 const Input: React.FC<Props> = (props: Props) => {
+
+  const handleEnableInput = (event: FocusEvent<HTMLInputElement>) =>
+    event.target.readOnly = false;
+
   return (
     <div className={Styles.inputWrap}>
-      <input {...props} />
+      <input {...props} readOnly onFocus={handleEnableInput} />
       <span>🔴</span>
     </div>
   );
